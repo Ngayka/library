03cf11376ec59a987d4fe2c5261e12298a3013fe
Ngayka <ngayka@gmail.com>
2025-05-27 10:54:02 +0300

    change urls catalog

--- library/urls.py
+++ library/urls.py
@@ -21,7 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-        path("catalog/", include("catalog.urls", namespace="catalog")),
+        path("", include("catalog.urls", namespace="catalog")),
         path("registration/", include("django.contrib.auth.urls")),
         #path("registration/", include("registration.urls", namespace="registration")),
         path("__debug__/", include("debug_toolbar.urls")),
